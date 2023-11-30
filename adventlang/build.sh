58549148310468eb8f420a3e49909506aff26bff
Kelley van Evert <hello@klve.nl>
2023-11-30 22:39:05 +0100

    command

---
+++ adventlang/build.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+cargo build --release
+cp ./target/release/adventlang_2023 ~/bin/al
--- adventlang/src/main.rs
+++ adventlang/src/main.rs
@@ -1,9 +1,11 @@
 #![feature(let_chains)]
 #![feature(iterator_try_reduce)]
 use std::{
-    env, fs,
+    fs,
     io::{self, Read},
+    path::PathBuf,
     process::exit,
+    time::Instant,
 };
 
 pub mod ast;
@@ -11,43 +13,68 @@ pub mod parse;
 pub mod parser_combinators;
 pub mod runtime;
 
+use clap::{Parser, Subcommand};
 use parse::parse_document;
 use runtime::execute;
 
+#[derive(Parser)]
+#[command()]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Execute a script
+    Run {
+        #[arg(short, long)]
+        /// Print out how long parsing and executing took
+        timings: bool,
+
+        /// File to run
+        file: PathBuf,
+    },
+}
+
 fn main() {
-    let Some(filename) = env::args().nth(1) else {
-        eprintln!("Pass name of file to execute");
-        exit(1);
-    };
+    let cli = Cli::parse();
 
-    let Ok(contents) = fs::read_to_string(filename) else {
-        eprintln!("Could not read file");
-        exit(2);
-    };
+    match cli.command {
+        Commands::Run { timings, file } => {
+            let Ok(contents) = fs::read_to_string(&file) else {
+                eprintln!("Could not read file: {}", file.display());
+                exit(1);
+            };
 
-    let Some(doc) = parse_document(&contents) else {
-        eprintln!("Could not parse");
-        exit(3);
-    };
+            let t0 = Instant::now();
+            let Some(doc) = parse_document(&contents) else {
+                eprintln!("Could not parse document");
+                exit(2);
+            };
+            // println!("Parsed: {doc:?}");
+            if timings {
+                eprintln!("Parsed in {:?}", t0.elapsed());
+            }
 
-    // println!("Parsed: {doc:?}");
+            let stdin = if atty::is(atty::Stream::Stdin) {
+                "".to_string()
+            } else {
+                let mut str = String::new();
+                io::stdin().read_to_string(&mut str).unwrap();
+                str
+            };
 
-    let stdin = if atty::is(atty::Stream::Stdin) {
-        "".to_string()
-    } else {
-        let mut str = String::new();
-        io::stdin().read_to_string(&mut str).unwrap();
-        str
-    };
-
-    match execute(&doc, stdin) {
-        Err(runtime_err) => {
-            eprintln!("Runtime error: {}", runtime_err.0);
-            exit(4);
-        }
-        Ok(value) => {
-            println!("{value}");
-            exit(0);
+            let t0 = Instant::now();
+            let result = execute(&doc, stdin);
+            if timings {
+                eprintln!("Executed in {:?}", t0.elapsed());
+                eprintln!("===");
+            }
+            if let Err(runtime_err) = result {
+                eprintln!("Runtime error: {}", runtime_err.0);
+                exit(3);
+            }
         }
     }
 }
