a064c0edd5fd2084cf8602c95e32fc2229ce0d7f
Kelley van Evert <hello@klve.nl>
2023-12-01 13:00:20 +0100

    added aoc day 1 to runtime tests

--- adventlang/src/runtime.rs
+++ adventlang/src/runtime.rs
@@ -1242,18 +1242,14 @@ pub fn execute(doc: &Document, stdin: String) -> Result<Value, RuntimeError> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{
-        ast::{Argument, Identifier},
-        parse::{identifier, parse_document},
-        parser_combinators::{alt, recognize, seq, tag, Parser},
-    };
-
-    fn id(id: &str) -> Identifier {
-        Identifier(id.into())
-    }
+    use crate::*;
 
     fn list(elements: Vec<Value>) -> Value {
         Value::List(elements)
+    }
+
+    fn tuple(elements: Vec<Value>) -> Value {
+        Value::Tuple(elements)
     }
 
     fn int(n: i64) -> Value {
@@ -1270,6 +1266,103 @@ mod tests {
         assert_eq!(
             execute(&parse_document("range(6, 2)").unwrap(), "".into()),
             Ok(list(vec![int(6), int(5), int(4), int(3)]))
+        );
+    }
+
+    #[test]
+    fn aoc_day01() {
+        let document = r#"
+
+let digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+fn is_digit(s) {
+  s :in digits
+}
+
+fn lines(text) {
+  text :split "\n"
+}
+
+let nums = [
+  ("0", "0"),
+  ("1", "1"),
+  ("2", "2"),
+  ("3", "3"),
+  ("4", "4"),
+  ("5", "5"),
+  ("6", "6"),
+  ("7", "7"),
+  ("8", "8"),
+  ("9", "9"),
+  ("zero", "0"),
+  ("one", "1"),
+  ("two", "2"),
+  ("three", "3"),
+  ("four", "4"),
+  ("five", "5"),
+  ("six", "6"),
+  ("seven", "7"),
+  ("eight", "8"),
+  ("nine", "9"),
+  ("ten", "0"),
+  ("eleven", "1"),
+  ("twelve", "2"),
+  ("thirteen", "3"),
+  ("fourteen", "4"),
+  ("fifteen", "5"),
+  ("sixteen", "6"),
+  ("seventeen", "7"),
+  ("eighteen", "8"),
+  ("nineteen", "9"),
+  ("twenty", "0"),
+]
+
+fn solve(input) {
+  let values = input .lines :map |line| {
+    let digits = line .chars :filter is_digit
+    int(digits[0] + digits[-1])
+  }
+
+  values.sum
+}
+
+fn bonus(input) {
+  let values = input .lines :map |line| {
+    let digits = range(0, line.len)
+      :filter_map |i| {
+        nums :find_map |t| {
+          if (line.slice(i).starts_with(t[0])) {
+            t[1]
+          }
+        }
+      }
+
+    int(digits[0] + digits[-1])
+  }
+
+  values.sum
+}
+
+let solution = solve("1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet");
+
+let bonus_solution = bonus("two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen");
+
+(solution, bonus_solution)
+
+"#;
+
+        assert_eq!(
+            execute(&parse_document(document).unwrap(), "".into()),
+            Ok(tuple(vec![int(142), int(281)]))
         );
     }
 }
